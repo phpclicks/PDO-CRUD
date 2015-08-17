226bff3c17a134bab0baab624d3fde9228caf3e5
phpclicks <phpclicks@gmail.com>
2015-08-17 14:42:15 +0500

    upload code for PDO CRUD

---
+++ footer.php
@@ -0,0 +1,33 @@
+    
+<div class="footer">
+	
+	<div class="footer-inner">
+		
+		<div class="container">
+			
+			<div class="row">
+				
+    			<div class="span12">
+    				&copy; 2013 <a href="http://www.egrappler.com/">Bootstrap Responsive Admin Template</a>.
+    			</div> <!-- /span12 -->
+    			
+    		</div> <!-- /row -->
+    		
+		</div> <!-- /container -->
+		
+	</div> <!-- /footer-inner -->
+	
+</div> <!-- /footer -->
+    
+
+
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/student.js"></script>
+	
+<script src="js/bootstrap.js"></script>
+<script src="js/base.js"></script>
+
+
+  </body>
+
+</html>
---
+++ include/config.php
@@ -0,0 +1,3 @@
+<?php
+$db_con = new PDO('mysql:host=localhost;dbname=php_clicks', 'root', '');
+?>
